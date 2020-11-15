f0ffb2025b696f75741725a6338e41cd5c8dfd53
root <root@auth.gscsite.net>
2020-11-15 05:48:28 +0000

    adding phpldapadmin

--- ldap.sh
+++ ldap.sh
@@ -27,7 +27,9 @@ echo "==========================================================================
 echo " "
 read -p 'Please enter new user name: ' ldapname
 echo " "
-if [ -z "$ldapname" ]
+read -p 'Please enter your email: ' my_email
+
+if [ -z "$ldapname" ] || [ -z "$my_email" ]
 then
     echo 'Inputs cannot be blank please try again!'
     exit 0
@@ -51,3 +53,25 @@ sudo ldapsearch -Y EXTERNAL -H ldapi:/// -b cn=config "(objectClass=olcGlobal)" 
 sudo echo "local4.* /var/log/slapd.log" >> /etc/rsyslog.conf
 sudo systemctl restart rsyslog
 sudo systemctl restart slapd
+
+sudo apt install curl apt-transport-https ca-certificates php-fpm php-mbstring php-xmlrpc php-soap php-apcu php-smbclient php-ldap php-redis php-gd php-xml php-intl php-json php-imagick php-mysql php-cli php-ldap php-zip php-curl php-dev libmcrypt-dev php-pear php-ldap nginx-full certbot python-certbot-nginx python3-certbot-nginx -y
+
+cp ./phpldapadmin /etc/nginx/sites-available/
+sudo sed -i "s/your.domanin.com/`hostname`/g" /etc/nginx/sites-available/phpldapadmin
+sudo rm /etc/nginx/sites-enabled/default
+sudo ln -s /etc/nginx/sites-available/phpldapadmin /etc/nginx/sites-enabled/
+
+certbot --nginx --agree-tos --redirect --staple-ocsp --email $my_email -d `hostname`
+sudo systemctl stop nginx
+sudo ps aux  |  grep -i nginx  |  awk '{print $2}' | xargs sudo kill -9
+
+sudo git clone https://github.com/leenooks/phpLDAPadmin.git /var/www/html/phpldapadmin
+sudo cp ./config.php   /var/www/html/phpldapadmin/config/config.php 
+configphp001='$servers->setValue('server','base',array('$dcldap'));'   
+echo $configphp001 | sudo tee -a /var/www/html/phpldapadmin/config/config.php 
+sudo sed -i "s|$servers->setValue('login','bind_id','');|$servers->setValue('login','bind_id','$aldap');|g"  /var/www/html/phpldapadmin/config/config.php 
+
+nginx -t
+systemctl start nginx
+systemctl status nginx
+
