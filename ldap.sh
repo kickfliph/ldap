b3364509409a1c0b52cef050bc0df70df5323196
root <root@auth.gscsite.net>
2020-11-16 03:08:34 +0000

    Modification of the config phpldapadmin file

--- ldap.sh
+++ ldap.sh
@@ -14,6 +14,14 @@ sudo apt-get install ldap-utils -y
 
 dcldap=`sudo slapcat | grep dc | head -n1 | awk '{print $2}'`
 aldap=`sudo slapcat | grep admin | head -n1 | awk '{print $2}'`
+
+sudo apt install curl apt-transport-https ca-certificates php-fpm php-mbstring php-xmlrpc php-soap php-apcu php-smbclient php-ldap php-redis php-gd php-xml php-intl php-json php-imagick php-mysql php-cli php-ldap php-zip php-curl php-dev libmcrypt-dev php-pear php-ldap nginx-full certbot python-certbot-nginx python3-certbot-nginx -y
+
+cp ./phpldapadmin /etc/nginx/sites-available/
+sudo sed -i "s/your.domanin.com/`hostname`/g" /etc/nginx/sites-available/phpldapadmin
+sudo rm /etc/nginx/sites-enabled/default
+sudo ln -s /etc/nginx/sites-available/phpldapadmin /etc/nginx/sites-enabled/
+
 echo "================================================================================================================================"
 echo " "
 read -p 'Please enter your email: ' my_email
@@ -25,33 +33,27 @@ then
 fi
 echo " "
 
-sudo cp ./enable-ldap-log.ldif /etc/ldap/
-sudo ldapmodify -Y external -H ldapi:/// -f enable-ldap-log.ldif 
-sudo ldapsearch -Y EXTERNAL -H ldapi:/// -b cn=config "(objectClass=olcGlobal)" olcLogLevel -LLL
-sudo echo "local4.* /var/log/slapd.log" >> /etc/rsyslog.conf
-sudo systemctl restart rsyslog
-sudo systemctl restart slapd
-
-sudo apt install curl apt-transport-https ca-certificates php-fpm php-mbstring php-xmlrpc php-soap php-apcu php-smbclient php-ldap php-redis php-gd php-xml php-intl php-json php-imagick php-mysql php-cli php-ldap php-zip php-curl php-dev libmcrypt-dev php-pear php-ldap nginx-full certbot python-certbot-nginx python3-certbot-nginx -y
-
-cp ./phpldapadmin /etc/nginx/sites-available/
-sudo sed -i "s/your.domanin.com/`hostname`/g" /etc/nginx/sites-available/phpldapadmin
-sudo rm /etc/nginx/sites-enabled/default
-sudo ln -s /etc/nginx/sites-available/phpldapadmin /etc/nginx/sites-enabled/
-
 certbot --nginx --agree-tos --redirect --staple-ocsp --email $my_email -d `hostname`
 sudo systemctl stop nginx
 sudo ps aux  |  grep -i nginx  |  awk '{print $2}' | xargs sudo kill -9
 
 sudo git clone https://github.com/leenooks/phpLDAPadmin.git /var/www/html/phpldapadmin
 sudo cp ./config.php   /var/www/html/phpldapadmin/config/config.php 
-configphp001='$servers->setValue('server','base',array('$dcldap'));'   
+configphp001='$servers->setValue('server','base',array('$dcldap'));'
+configphp002='$servers->setValue('login','bind_id','$aldap');'
 echo $configphp001 | sudo tee -a /var/www/html/phpldapadmin/config/config.php 
-sudo sed -i "s|$servers->setValue('login','bind_id','');|$servers->setValue('login','bind_id','$aldap');|g"  /var/www/html/phpldapadmin/config/config.php 
+echo $configphp002 | sudo tee -a /var/www/html/phpldapadmin/config/config.php 
+sudo sed -i "s|My LDAP Server|$dcldap|g"  /var/www/html/phpldapadmin/config/config.php
 
 nginx -t
 systemctl start nginx
 systemctl status nginx
 
 
+sudo cp ./enable-ldap-log.ldif /etc/ldap/
+sudo ldapmodify -Y external -H ldapi:/// -f enable-ldap-log.ldif 
+sudo ldapsearch -Y EXTERNAL -H ldapi:/// -b cn=config "(objectClass=olcGlobal)" olcLogLevel -LLL
+sudo echo "local4.* /var/log/slapd.log" >> /etc/rsyslog.conf
+sudo systemctl restart rsyslog
+sudo systemctl restart slapd
 
